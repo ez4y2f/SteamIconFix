2c32f945898323e59e64f5297a256b883b824ff9
ez4y2f <xiaobaoyzf@hotmail.com>
2025-11-27 22:49:24 +0800

    Fixed issue #7; Updated Building Toolchain; HTTPS Supported.

--- SteamIconFix.cpp
+++ SteamIconFix.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 #include <winsock2.h>
 #include <windows.h>
-#include <dirent.h>
 #include <wininet.h>
 #include <tchar.h>
 #include <sys/stat.h>
 #include <Shlobj.h>
 #include <format>
 #include <vector>
-
-#define CURL_STATICLIB
+#include <string>
 
 #include "include/curl.h"
 // for windows only :(
 
 using namespace std;
 
-BOOL isFileExists(const string &path) {
-    DWORD dwAttr = GetFileAttributes((LPCSTR) path.c_str());
-    if (dwAttr == 0xFFFFFFFF) return false;
-    return true;
+BOOL isFileExists(const wstring &path) {
+    DWORD dwAttr = GetFileAttributesW(path.c_str());
+    if (dwAttr == INVALID_FILE_ATTRIBUTES) return FALSE;
+    return TRUE;
 }
-
-/*
-BOOL downloadFile(const string& url, const string& path) {
-    if(!isFileExists(path)) return false;
-    if(URLDownloadToFile(nullptr, (LPCSTR)url.c_str(), (LPCSTR)path.c_str(), 0, nullptr) == S_OK) return true;
-    return false;
-}
-*/
 
 size_t curlWriteFunc(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return fwrite(ptr, size, nmemb, stream);
 }
 
-int downloadFile(const string &url, const string &path) {
+int downloadFile(const string &url, const wstring &path) {
     CURL *curl = curl_easy_init();
-    long responseCode;
+    long responseCode = 0;
     if (curl) {
-        FILE *ofile = fopen(path.c_str(), "wb");
+        FILE *ofile = _wfopen(path.c_str(), L"wb");
+        if (!ofile) {
+            curl_easy_cleanup(curl);
+            return -2;
+        }
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, ofile);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteFunc);
@@ -49,8 +43,7 @@ int downloadFile(const string &url, const string &path) {
         curl_easy_cleanup(curl);
 
         if (res != CURLE_OK) return res;
-        if (responseCode >=400)
-            return responseCode;
+        if (responseCode >= 400) return (int)responseCode;
         return 0;
     }
     return -1;
@@ -102,51 +95,61 @@ void logerr(const char *str) {
     setclr(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 }
 
-bool getDirUrlFiles(const string &path, vector<string> &files) {
-    DIR *dir;
-    dirent *ptr;
-    struct stat s{};
-    string fullDir;
-    bool result;
-
-    if ((dir = opendir(path.c_str())) == nullptr) {
-        cerr << "E[GetDirFiles] Failed open dir." << endl;
-        return false;
-    }
-
-    while ((ptr = readdir(dir)) != nullptr) {
-        if (strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0) continue; // cur or pat dir
-        fullDir = path + '\\' + ptr->d_name;
-        stat(fullDir.c_str(), &s);//use full dir is true
-        if (s.st_mode & S_IFDIR) continue;
-        if (strcmp(ptr->d_name + strlen(ptr->d_name) - 4, ".url") != 0) continue;//not url file
-        result = false;
-        for (auto &file: files) {
-            if (fullDir.substr(fullDir.find_last_of('\\') + 1) == file.substr(file.find_last_of('\\') + 1)) {
-                result = true;
-                break;
-            }
-        }
-        if (!result) files.push_back(fullDir);
-    }
-    closedir(dir);
-    return true;
-}
-
 // thanks @Zinc-in
 string wstring2string(const wstring &wstr) {
     string result;
     int len = WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS,
-                                  wstr.c_str(), wstr.size(),
+                                  wstr.c_str(), (int)wstr.size(),
                                   nullptr, 0, nullptr, nullptr);
+    if (len <= 0) return {};
     char *buffer = new char[len + 1];
     WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS,
-                        wstr.c_str(), wstr.size(),
+                        wstr.c_str(), (int)wstr.size(),
                         buffer, len, nullptr, nullptr);
     buffer[len] = '\0';
     result.append(buffer);
     delete[] buffer;
     return result;
+}
+
+bool getDirUrlFiles(const wstring &path, vector<wstring> &files) {
+    WIN32_FIND_DATAW ffd{};
+    wstring searchPath = path;
+    if (!searchPath.empty() && searchPath.back() != L'\\')
+        searchPath += L'\\';
+    searchPath += L"*.url";
+
+    HANDLE hFind = FindFirstFileW(searchPath.c_str(), &ffd);
+    if (hFind == INVALID_HANDLE_VALUE) {
+        cerr << "E[GetDirFiles] Failed open dir." << endl;
+        return false;
+    }
+
+    do {
+        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) continue;
+
+        wstring fullPath = path;
+        if (!fullPath.empty() && fullPath.back() != L'\\')
+            fullPath += L'\\';
+        fullPath += ffd.cFileName;
+
+        bool result = false;
+        for (auto &file : files) {
+            auto pos1 = fullPath.find_last_of(L'\\');
+            auto pos2 = file.find_last_of(L'\\');
+            wstring name1 = (pos1 == wstring::npos) ? fullPath : fullPath.substr(pos1 + 1);
+            wstring name2 = (pos2 == wstring::npos) ? file : file.substr(pos2 + 1);
+            if (name1 == name2) {
+                result = true;
+                break;
+            }
+        }
+        if (!result) files.push_back(fullPath);
+
+    } while (FindNextFileW(hFind, &ffd));
+
+    FindClose(hFind);
+    return true;
 }
 
 int main() {
@@ -158,123 +161,174 @@ int main() {
     setclr(6);
     cout << ">_D3bug the w0r1d.    Visit https://y2f.xyz for more information." << endl;
     setclr(15);
-    /*
-    char dirarr[MAX_PATH];
-    getcwd(dirarr, MAX_PATH);
-    string dir = dirarr;
-    dir = dir.substr(dir.find_last_of('\\') + 1, dir.length() - 1);
-    cout << "Current running in dir " << dir << endl;*///no need
 
-    string progFilesVar;
-    DWORD size = MAX_PATH;
+    wstring progFilesVar;
+    DWORD size = MAX_PATH * sizeof(WCHAR);
     auto *buffer = new WCHAR[MAX_PATH];
     if (!SUCCEEDED(RegGetValueW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node\\Valve\\Steam", L"InstallPath", RRF_RT_ANY,
                                 nullptr, (PVOID) buffer, &size))) {
         logerr("E Steam is not installed, exiting...");
         system("pause");
-        exit(0);
+        delete[] buffer;
+        return 0;
     }
 
-    progFilesVar = wstring2string(buffer);
+    progFilesVar = buffer;
     delete[] buffer;
 
 
-    WCHAR *desktopVar;
+    PWSTR desktopVar = nullptr;
     if (!SUCCEEDED(SHGetKnownFolderPath(FOLDERID_Desktop, 0, nullptr, &desktopVar))) {
         logerr("E Cannot find Desktop, exiting...");
         system("pause");
-        exit(0);
+        return 0;
     }
 
-    WCHAR *startMenuVar;
-    if (!SUCCEEDED(SHGetKnownFolderPath(FOLDERID_Programs, 0, nullptr, &startMenuVar))) {
+    PWSTR startMenuVar = nullptr;
+    HRESULT hrStart = SHGetKnownFolderPath(FOLDERID_Programs, 0, nullptr, &startMenuVar);
+    if (!SUCCEEDED(hrStart)) {
         logwrn("W Cannot find Start menu, skipped");
     }
 
-    string desktopDir = wstring2string(desktopVar);
-    string startMenuDir = wstring2string(startMenuVar) + R"(\Steam)";
-    string steamiconDir = progFilesVar + R"(\steam\games)";
+    wstring desktopDir = desktopVar;
+    CoTaskMemFree(desktopVar);
+
+    wstring startMenuDir;
+    if (SUCCEEDED(hrStart)) {
+        startMenuDir = startMenuVar;
+        CoTaskMemFree(startMenuVar);
+        if (!startMenuDir.empty() && startMenuDir.back() != L'\\')
+            startMenuDir += L'\\';
+        startMenuDir += L"Steam";
+    }
+
+    wstring steamiconDir = progFilesVar;
+    if (!steamiconDir.empty() && steamiconDir.back() != L'\\')
+        steamiconDir += L'\\';
+    steamiconDir += L"steam\\games";
 
     if (isFileExists(steamiconDir)) {
-        cout << "Found Steam icon dir in " << steamiconDir << endl;
+        cout << "Found Steam icon dir in " << wstring2string(steamiconDir) << endl;
     } else {
         logwrn("W Cannot find steam icon dir, manual input>_");
-        getline(cin, steamiconDir);
+        string input;
+        getline(cin, input);
+        if (input.empty()) {
+            logerr("E Path not exist. Exiting...");
+            system("pause");
+            return 0;
+        }
+        steamiconDir = wstring(input.begin(), input.end());
         if (!isFileExists(steamiconDir)) {
             logerr("E Path not exist. Exiting...");
             system("pause");
-            exit(0);
+            return 0;
         }
     }
 
     if (isFileExists(desktopDir)) {
-        cout << "Found Desktop in " << desktopDir << endl;
+        cout << "Found Desktop in " << wstring2string(desktopDir) << endl;
     } else {
         logwrn("W Cannot find desktop dir, manual input>_");
-        getline(cin, desktopDir);
+        string input;
+        getline(cin, input);
+        if (input.empty()) {
+            logerr("E Path not exist. Exiting...");
+            system("pause");
+            return 0;
+        }
+        desktopDir = wstring(input.begin(), input.end());
         if (!isFileExists(desktopDir)) {
             logerr("E Path not exist. Exiting...");
             system("pause");
-            exit(0);
+            return 0;
         }
     }
-    if (isFileExists(startMenuDir)) {
-        cout << "Found Steam Start Menu Shortcut in " << startMenuDir << endl;
+
+    if (!startMenuDir.empty() && isFileExists(startMenuDir)) {
+        cout << "Found Steam Start Menu Shortcut in " << wstring2string(startMenuDir) << endl;
     } else {
         logwrn("W Cannot find StartMenu Dir, skipping...");
     }
 
-    string cdnChoose;
+    int cdnChoose = -1;
 
     cout << endl;
     logui("==Type what CDN u wanna use==");
-    logui("0 -> akamai");
+    logui("0 -> fastly");
     logui("1 -> cloudflare");
 
-    while ((cdnChoose != "0") && (cdnChoose != "1")) {
+    while ((cdnChoose != 0) && (cdnChoose != 1)) {
         cout << "CDN[enter for 0]";
-        getline(cin, cdnChoose);
-        if (cdnChoose.empty()) {
-            cdnChoose = "0";
-            break;
+        string cdnChooseStr;
+        getline(cin, cdnChooseStr);
+        if (cdnChooseStr.length() > 1) {
+            cout << "Input Invalid" << endl;
+            continue;
         }
-        if ((cdnChoose != "0") && (cdnChoose != "1")) cout << "Input invalid." << endl;
+        if (cdnChooseStr.empty()) cdnChooseStr = "0";
+        char cdnChooseChar = cdnChooseStr[0];
+        if (!(cdnChooseChar < '2' && cdnChooseChar >= '0')) {
+            cout << "Input Invalid" << endl;
+            continue;
+        }
+        cdnChoose = cdnChooseChar - '0';
     }
     cout << endl;
 
-    cdnChoose = cdnChoose == "0" ? "akamai" : "cloudflare";
-    logui(vformat("Current using CDN {}", make_format_args(cdnChoose)).c_str());
-
-    vector<string> files;
+    vector<wstring> files;
     getDirUrlFiles(desktopDir, files);
-    getDirUrlFiles(startMenuDir, files);
-    string url;
-    string iconfile;
-    char urlbuf[256]; // vars in ini(.url)
-    char iconbuf[MAX_PATH];
+    if (!startMenuDir.empty() && isFileExists(startMenuDir)) {
+        getDirUrlFiles(startMenuDir, files);
+    }
 
-    string iconurl = "http://cdn.{}.steamstatic.com/steamcommunity/public/images/apps/";
-    iconurl = vformat(iconurl, make_format_args(cdnChoose));
+    wchar_t urlbuf[256]; // vars in ini(.url)
+    wchar_t iconbuf[MAX_PATH];
 
-    for (const auto &file: files) {
-        GetPrivateProfileString(_T("InternetShortcut"), _T("URL"), nullptr, urlbuf, sizeof(urlbuf), file.c_str());
-        GetPrivateProfileString(_T("InternetShortcut"), _T("IconFile"), nullptr, iconbuf, sizeof(iconbuf),
-                                file.c_str());
-        url = urlbuf;
-        if (url.find(':') > url.length() || url.find_last_of('/') > url.length()) continue;
-        if (strcmp(url.substr(0, url.find(':')).c_str(), "steam") != 0) continue; // not a steam shortcut
-        iconfile = iconbuf;
-        if (iconfile.find_last_of('\\') > iconfile.length()) {
+    string iconurl;
+    if (cdnChoose == 0) iconurl = "https://shared.fastly.steamstatic.com/community_assets/images/apps/";
+    if (cdnChoose == 1) iconurl = "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/";
+
+    for (const auto &file : files) {
+        GetPrivateProfileStringW(L"InternetShortcut", L"URL", nullptr, urlbuf, ARRAYSIZE(urlbuf), file.c_str());
+        GetPrivateProfileStringW(L"InternetShortcut", L"IconFile", nullptr, iconbuf, ARRAYSIZE(iconbuf),
+                                 file.c_str());
+
+        wstring urlW = urlbuf;
+        wstring iconW = iconbuf;
+
+        if (urlW.empty()) continue;
+
+        auto colonPos = urlW.find(L':');
+        auto slashPos = urlW.find_last_of(L'/');
+        if (colonPos == wstring::npos || slashPos == wstring::npos) continue;
+
+        if (urlW.substr(0, colonPos) != L"steam") continue; // not a steam shortcut
+
+        if (iconW.find_last_of(L'\\') == wstring::npos) {
             logerr("E invalid shortcut");
             continue;
         }
-        url = url.substr(url.find_last_of('/') + 1);
-        iconfile = iconfile.substr(iconfile.find_last_of('\\') + 1);
-        cout << "Find Steam Shortcut " << file.substr(file.find_last_of('\\') + 1) << " with appid " << url << endl;
-        string dwnurl = iconurl + url + "/" + iconfile;
 
-        cout << "- try to download icon from " << dwnurl << " to " << steamiconDir + "\\" + iconfile << endl;
-        int res = downloadFile(dwnurl, steamiconDir + "\\" + iconfile);
+        wstring appIdW = urlW.substr(slashPos + 1);
+        wstring iconFileNameW = iconW.substr(iconW.find_last_of(L'\\') + 1);
+
+        string appId = wstring2string(appIdW);
+        string iconFileName = wstring2string(iconFileNameW);
+
+        cout << "Find Steam Shortcut "
+             << wstring2string(file.substr(file.find_last_of(L'\\') + 1))
+             << " with appid " << appId << endl;
+
+        string dwnurl = iconurl + appId + "/" + iconFileName;
+
+        wstring dstPath = steamiconDir;
+        if (!dstPath.empty() && dstPath.back() != L'\\')
+            dstPath += L'\\';
+        dstPath += iconFileNameW;
+
+        cout << "- try to download icon from " << dwnurl << " to " << wstring2string(dstPath) << endl;
+        int res = downloadFile(dwnurl, dstPath);
 
         bool terminateFlag = false; // terminate for error not-to-skip
 
@@ -298,9 +352,9 @@ int main() {
         }
         if (terminateFlag){
             logerr("E deleting downloaded files...");
-            system(("del \"" + steamiconDir + "\\" + iconfile + "\"").c_str());
+            DeleteFileW(dstPath.c_str());
             system("pause");
-            exit(0);
+            return 0;
         }
     }
 
